802a91bf7a4fc99aa2d78116cd8eb69dd54fde98
Ryan O'Toole <otoolerj@gmail.com>
2018-03-20 21:38:01 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,6 @@
 def unsafe?(speed)
 	if speed > 60 || speed < 40
-		return true 
+		return true
 	else
 		return false
 	end
@@ -10,5 +10,5 @@ end
 
 
 def not_safe?(speed)
-
+	40 > speed || 60 < speed ? true : false 
 end
